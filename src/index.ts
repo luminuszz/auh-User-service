9f33596718b5d7633ff5d8087cf41efbb6f9f7ba
daviribeiro <luminuszz43@gmail.com>
2021-04-10 15:04:36 -0300

    ajusts

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ export async function start(server: FastifyInstance) {
 		server.register(routes);
 		server.register(UsersRoutes);
 
+		server.get('/', async (_, res) => {
+			return res.send({
+				message: 'server is workig',
+			});
+		});
+
 		await server.listen(process.env.PORT || 3000, '0.0.0.0');
 	} catch (error) {
 		server.log.error(error);
