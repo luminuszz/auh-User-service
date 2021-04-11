de271e87753f78e98acea97cb11b36be3e194a96
daviribeiro <luminuszz43@gmail.com>
2021-04-11 18:14:51 -0300

    ident file

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import fastify, { FastifyInstance } from 'fastify';
-import { routes } from './routes';
-import { UsersRoutes } from './routes/users.route';
-import { connectionDb } from './database/connection';
+import { FastifyInstance } from "fastify";
+import { routes } from "./routes";
+import { UsersRoutes } from "./routes/users.route";
+import { connectionDb } from "./database/connection";
 
 export async function start(server: FastifyInstance) {
-	try {
-		server.register(connectionDb);
-		server.register(routes);
-		server.register(UsersRoutes);
+    try {
+        server.register(connectionDb);
+        server.register(routes);
+        server.register(UsersRoutes);
 
-		server.get('/', async (_, res) => {
-			return res.send({
-				message: 'server is workig',
-			});
-		});
+        server.get("/", async (_, res) => {
+            return res.send({
+                message: "server is workig",
+            });
+        });
 
-		await server.listen(process.env.PORT || 3000, '0.0.0.0');
-	} catch (error) {
-		server.log.error(error);
-	}
+        await server.listen(process.env.PORT || 3000, "0.0.0.0");
+    } catch (error) {
+        server.log.error(error);
+    }
 }
